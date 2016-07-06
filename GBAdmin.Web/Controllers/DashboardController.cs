ef1893bf9d0f58fd13a1adfe06b82d277bd1a7cc
ajaynbiswas <ajayn.biswas@gmail.com>
2016-07-06 21:30:09 +0530

    dashboard in progress

--- GBAdmin.Web/Controllers/DashboardController.cs
+++ GBAdmin.Web/Controllers/DashboardController.cs
@@ -4,18 +4,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GBAdmin.Web.Services;
+using GBAdmin.Web.Models;
 
 namespace GBAdmin.Web.Controllers
 {
     public class DashboardController : Controller
     {
+        GB.Data.DBEntity.GaddibabaEntities GBContext = new GB.Data.DBEntity.GaddibabaEntities();
+
         [Route("Dashboard", Name = RouteNames.Dashboard)]
         public ActionResult Index()
         {
-            return View();
+            var UserID = SessionManager.GetSessionUser().Id;
+            var UserList = GBContext.Users.Where(m => m.CreatedBy == UserID).ToList();
+            DashboardViewModel DashboardViewModel = new DashboardViewModel();
+            foreach (var user in UserList)
+            {
+                string UserName = user.FirstName + " " + user.LastName;
+                int newCount = GBContext.DriverDetails.Count(m => m.CreatedBy == user.UserID && m.DriverStatusID == 1);
+                int approvedCount = GBContext.DriverDetails.Count(m => m.CreatedBy == user.UserID && m.DriverStatusID == 2);
+                int rejectedCount = GBContext.DriverDetails.Count(m => m.CreatedBy == user.UserID && m.DriverStatusID == 3);
+                int attachedToUberCount = GBContext.DriverDetails.Count(m => m.CreatedBy == user.UserID && m.DriverStatusID == 8);
+
+                if(newCount > 0)
+                 DashboardViewModel.New.Add(new KeyValuePair<string,int>(UserName,newCount ));
+
+                if(approvedCount > 0)
+                    DashboardViewModel.Approved.Add(new KeyValuePair<string, int>(UserName, approvedCount));
+
+                if(rejectedCount > 0)
+                    DashboardViewModel.Rejected.Add(new KeyValuePair<string, int>(UserName, rejectedCount));
+
+                if(attachedToUberCount > 0)
+                    DashboardViewModel.AttachedToUber.Add(new KeyValuePair<string, int>(UserName, attachedToUberCount));
+            }
+            return View(DashboardViewModel);
         }
 
         
-
     }
 }
---
+++ GBAdmin.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GBAdmin.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public IDictionary<string, int> AttachedToUber { get; set; }
+        public IDictionary<string, int> New { get; set; }
+        public IDictionary<string, int> Approved { get; set; }
+        public IDictionary<string, int> Rejected { get; set; }
+
+    }
+}
