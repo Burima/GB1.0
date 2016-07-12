3871e55456ccfb45667faacde12764a1bb6070e3
liakat <liakat.mondol@valuelabs.com>
2016-07-12 18:36:00 +0530

    error logging in progress

---
+++ GBAdmin.Web/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GBAdmin.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index(int status = 0, Exception error = null)
+        {
+            Response.StatusCode = status;
+            //return View(status);
+            return RedirectToAction("LogOff", "Account");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
--- GBAdmin.Web/Global.asax.cs
+++ GBAdmin.Web/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace GBAdmin.Web
 {
@@ -16,6 +17,35 @@ namespace GBAdmin.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var routeData = new RouteData();
+            var ex = Server.GetLastError().GetBaseException();
+            //Added logging Error in DB
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Error("ApplicationError", ex);
+            //End
+            Server.ClearError();
+            //var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+            routeData.Values.Add("action", "Index");
+
+            if (ex.GetType() == typeof(HttpException))
+            {
+                var httpException = (HttpException)ex;
+                var code = httpException.GetHttpCode();
+                routeData.Values.Add("status", code);
+            }
+            else
+            {
+                routeData.Values.Add("status", 500);
+            }
+
+            routeData.Values.Add("error", ex);
+            IController errorController = new GBAdmin.Web.Controllers.ErrorController();
+            errorController.Execute(new RequestContext(new HttpContextWrapper(Context), routeData));
+
         }
     }
 }
